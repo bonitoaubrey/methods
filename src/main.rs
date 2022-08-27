71ffdd3a960ccde698fa5ccb92690a678526e613
Bonito Aubrey <bonitoaubrey@gmail.com>
2022-08-27 16:46:23 +0300

    add square method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     height: u32,
@@ -15,6 +17,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -28,6 +37,8 @@ fn main() {
         height: 40,
     };
 
+    let rect3 = Rectangle::square(3);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
@@ -36,4 +47,6 @@ fn main() {
     println!("The rectangle has a nonzero width; it is {}", rect1.width());
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    println!("rect3 is: \n {:#?}", rect3);
 }
